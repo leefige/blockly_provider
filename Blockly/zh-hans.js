a09745e45b1951e741489b9906f5c1cc8e05b282
李逸飞 <liyifei15@mails.tsinghua.edu.cn>
2017-10-18 23:02:22 +0800

    add chinese name

--- Blockly/zh-hans.js
+++ Blockly/zh-hans.js
@@ -424,3 +424,11 @@ goog.require('Blockly.Msg');
 /** @export */ Blockly.Msg.TEXTS_HUE = "160";
 /** @export */ Blockly.Msg.PROCEDURES_HUE = "290";
 /** @export */ Blockly.Msg.COLOUR_HUE = "20";
+
+/* ------------------------------------------------------------------- */
+
+/** @export */ Blockly.Msg.ACTIONS_HUE = "15";
+
+/** @export */ Blockly.Msg.ACTIONS_GO_TITLE = "前进";
+/** @export */ Blockly.Msg.ACTIONS_TURN_LEFT_TITLE = "左转";
+/** @export */ Blockly.Msg.ACTIONS_TURN_RIGHT_TITLE = "右转";
